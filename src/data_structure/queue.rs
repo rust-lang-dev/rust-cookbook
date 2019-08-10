7bd12ae8289a429011480994762cfc4fb861eaf5
its404 <benwk424@gmail.com>
2019-08-10 21:54:41 +1200

    add queue

---
+++ src/data_structure/queue.rs
@@ -0,0 +1,29 @@
+#[derive(Debug)]
+struct Queue<T> {
+    q: Vec<T>,
+}
+
+impl<T> Queue<T> {
+    fn new() -> Queue<T> {
+        Queue { q: Vec::new() }
+    }
+    fn push(&mut self, v: T) {
+        self.q.push(v)
+    }
+    fn pop(&mut self) -> Option<T> {
+        match self.q.len() {
+            0 => None,
+            _ => Some(self.q.remove(0)),
+        }
+    }
+}
+
+fn main() {
+    let mut queue = Queue::<i32>::new();
+    println!("{:?}", queue);
+    println!("{:?}", queue.pop());
+    queue.push(1);
+    println!("{:?}", queue);
+    println!("Pop: {:?}", queue.pop().unwrap());
+    println!("After pop: {:?}", queue);
+}
